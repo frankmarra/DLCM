5eb630d5136f6fa6f33aef0c13c10903fe879ea8
Frank Marra <frankrmarra@gmail.com>
2023-03-08 22:40:58 -0500

    create release working

---
+++ pages/create-release.js
@@ -0,0 +1,155 @@
+import { useState, useEffect } from 'react'
+import { supabase } from '@/utils/supabase'
+import { useUser } from '@/utils/context/user'
+
+const trueFalse = [
+  { id: 1, text: 'True', value: true },
+  { id: 2, text: 'False', value: false }
+]
+
+const releaseTypes = [
+  { id: 1, text: 'LP' },
+  { id: 2, text: 'EP' },
+  { id: 3, text: 'Single' },
+  { id: 4, text: 'Compilation' },
+  { id: 5, text: 'Soundtrack' },
+  { id: 6, text: 'Choose release type', isDisabled: true }
+]
+
+export default function CreateRelease() {
+  const { activeUser } = useUser()
+  const [newRelease, setNewRelease] = useState({
+    title: '',
+    artist: '',
+    label: '',
+    artwork: '',
+    downloadUrl: '',
+    pagePassword: '',
+    isPasswordProtected: trueFalse[1].value,
+    type: releaseTypes[5].text,
+    userId: ''
+  })
+
+  useEffect(() => {
+    if (activeUser) {
+      if (activeUser.type === 'Label') {
+        setNewRelease({
+          ...newRelease,
+          label: activeUser.name,
+          userId: activeUser.id
+        })
+      }
+      if (activeUser.type === 'Artist') {
+        setNewRelease({
+          ...newRelease,
+          artist: activeUser.name,
+          userId: activeUser.id
+        })
+      }
+    }
+  }, [activeUser])
+
+  const handleChange = (e) => {
+    setNewRelease({ ...newRelease, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const { data, error } = await supabase.from('releases').insert([newRelease])
+  }
+
+  return (
+    activeUser && (
+      <div className="create-release-wrapper">
+        <h1>Add new release</h1>
+        <form className="create-release-form" onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="title">Title</label>
+            <input
+              onChange={handleChange}
+              id="title"
+              type="text"
+              value={newRelease.title}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="artist">Artist</label>
+            {activeUser.type === 'Artist' ? (
+              <p>{activeUser.name}</p>
+            ) : (
+              <input
+                onChange={handleChange}
+                id="artist"
+                type="text"
+                value={newRelease.artist}
+              />
+            )}
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="label">Label</label>
+            {activeUser.type === 'Label' ? (
+              <p>{activeUser.name}</p>
+            ) : (
+              <input
+                onChange={handleChange}
+                id="label"
+                type="text"
+                value={newRelease.label}
+              />
+            )}
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="artwork">Artwork</label>
+            <input
+              onChange={handleChange}
+              id="artwork"
+              type="text"
+              value={newRelease.artwork}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="type">Type</label>
+            <select onChange={handleChange} id="type" value={newRelease.type}>
+              {releaseTypes.map((releaseType) => (
+                <option key={releaseType.id} value={releaseType.value}>
+                  {releaseType.text}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="isPasswordProtected">
+              Password protect release?
+            </label>
+            <select
+              onChange={handleChange}
+              id="isPasswordProtected"
+              value={newRelease.isPasswordProtected}
+            >
+              {trueFalse.map((bool) => (
+                <option key={bool.id} value={bool.value}>
+                  {bool.text}
+                </option>
+              ))}
+            </select>
+            {newRelease.isPasswordProtected ? (
+              <div className="input-wrapper">
+                <label htmlFor="pagePassword">Page password</label>
+                <input
+                  onChange={handleChange}
+                  id="pagePassword"
+                  type="password"
+                  value={newRelease.pagePassword}
+                />
+              </div>
+            ) : null}
+          </div>
+          <button type="submit" className="btn primary">
+            Create
+          </button>
+        </form>
+      </div>
+    )
+  )
+}
