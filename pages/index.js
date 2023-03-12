8f3ebc2d2ae81aaeae378e0ee591456a3f09e587
Ryan Mulligan <hexagoncircle@gmail.com>
2023-03-12 07:21:05 -0700

    Add svgr and starter icons collection

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
-import Login from '@/components/login'
-import Link from 'next/link'
+import Login from "@/components/login";
+import Link from "next/link";
+import IconRecord from "@/icons/vinyl-record.svg";
 
 export default function Home() {
   return (
     <div>
-      <h1>D L C M V 2</h1>
+      <h1>
+        <IconRecord aria-hidden="true" /> Download Code Manager
+      </h1>
       <Login />
       <p>
-        Not a member?{' '}
+        Not a member?{" "}
         <span>
           <Link href="/signup">Sign up!</Link>
         </span>
       </p>
     </div>
-  )
+  );
 }
