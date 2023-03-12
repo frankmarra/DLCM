c0e44b460c00f74c7be0431a3f1018221ac8aa5d
Ryan Mulligan <hexagoncircle@gmail.com>
2023-03-12 15:43:41 -0700

    Remove unnecessary code from dynamic pages

--- pages/artists/[slug].js
+++ pages/artists/[slug].js
@@ -1,29 +1,26 @@
-import { useEffect, useState } from 'react'
-import { supabase } from '@/utils/supabase'
-import { useRouter } from 'next/router'
-import { useUser } from '@/utils/context/user'
-import ProfileLayout from '@/components/ProfileLayout'
+import { useEffect, useState } from "react"
+import { supabase } from "@/utils/supabase"
+import { useRouter } from "next/router"
+import { useUser } from "@/utils/context/user"
+import ProfileLayout from "@/components/ProfileLayout"
 
 export async function getServerSideProps({ params }) {
   let { data: artist, error } = await supabase
-    .from('profiles')
-    .select('*')
-    .eq('type', 'Artist')
-    .eq('slug', params.slug)
+    .from("profiles")
+    .select("*")
+    .eq("type", "Artist")
+    .eq("slug", params.slug)
     .single()
 
   return { props: { artist } }
 }
 
 export default function ArtistPage({ artist }) {
-  const router = useRouter()
-  const { user, logout } = useUser()
   return artist ? (
     <ProfileLayout
       avatar={artist.avatar}
       name={artist.name}
       location={artist.location}
-      logout={logout}
     />
   ) : (
     <div>Loading...</div>
