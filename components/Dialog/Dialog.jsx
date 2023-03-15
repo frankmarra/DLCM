32c4df4f12cbfdff3625155733807f1726e2ab09
Ryan Mulligan <hexagoncircle@gmail.com>
2023-03-14 21:20:53 -0700

    Update styles

--- components/Dialog/Dialog.jsx
+++ components/Dialog/Dialog.jsx
@@ -1,11 +1,16 @@
 import { forwardRef } from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
+import cn from "classnames"
 import styles from "./Dialog.module.css"
 
 export const DialogContent = forwardRef(({ children, ...props }, ref) => (
   <DialogPrimitive.Portal>
     <DialogPrimitive.Overlay className={styles.overlay} />
-    <DialogPrimitive.Content className={styles.content} {...props} ref={ref}>
+    <DialogPrimitive.Content
+      className={cn(styles.content, "max-inline")}
+      {...props}
+      ref={ref}
+    >
       {children}
     </DialogPrimitive.Content>
   </DialogPrimitive.Portal>
