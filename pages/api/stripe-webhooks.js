aefd0509d7bcdfd8593f210589d17b969c01fd25
Frank Marra <frankrmarra@gmail.com>
2023-05-17 22:31:58 -0400

    successful webhook implementation

--- pages/api/stripe-webhooks.js
+++ pages/api/stripe-webhooks.js
@@ -1,49 +1,52 @@
 import Stripe from "stripe"
 import { supabase } from "@/utils/supabase"
-const endpointSecret =
-  "whsec_ed0b25f0aa25b273e709ee6e7feda8c64e0668e5cbe56a92ba95def7680f46ad"
+import { buffer } from "micro"
+
+export const config = { api: { bodyParser: false } }
+
 export default async function handler(req, res) {
   const stripe = Stripe(process.env.STRIPE_TEST_SECRET_KEY)
   const sig = req.headers["stripe-signature"]
+  const signingSecret = process.env.STRIPE_WEBHOOK_TEST_KEY
+  const reqBuffer = await buffer(req)
 
   let event
 
   try {
-    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret)
-    console.log("event: ", event)
+    event = stripe.webhooks.constructEvent(reqBuffer, sig, signingSecret)
   } catch (err) {
-    res.status(400).send(`Webhook Error: ${err.message}`)
-    return
+    console.log(err)
+    return res.status(400).send(`Webhook Error: ${err.message}`)
+  }
+  async function createdSub(customer) {
+    console.log(`subscription created for: ${customer}`)
+    await supabase
+      .from("profiles")
+      .update({ is_subscribed: true })
+      .eq("stripe_customer_id", customer)
   }
 
+  async function canceledSub(customer) {
+    console.log(`subscription canceled for customer: ${customer}`)
+    await supabase
+      .from("profiles")
+      .update({ is_subscribed: "false" })
+      .eq("stripe_customer_id", customer)
+  }
   switch (event.type) {
-    case "checkout.session.async_payment_failed":
-      const checkoutSessionAsyncPaymentFailed = event.data.object
-      // Then define and call a function to handle the event checkout.session.async_payment_failed
-      break
-    case "checkout.session.async_payment_succeeded":
-      const checkoutSessionAsyncPaymentSucceeded = event.data.object
-      // Then define and call a function to handle the event checkout.session.async_payment_succeeded
-      break
-    case "checkout.session.completed":
-      const checkoutSessionCompleted = event.data.object
-      // Then define and call a function to handle the event checkout.session.completed
-      break
-    case "checkout.session.expired":
-      const checkoutSessionExpired = event.data.object
-      // Then define and call a function to handle the event checkout.session.expired
-      break
     case "customer.subscription.created":
       const customerSubscriptionCreated = event.data.object
+      createdSub(customerSubscriptionCreated.customer)
       // Then define and call a function to handle the event customer.subscription.created
       break
     case "customer.subscription.deleted":
       const customerSubscriptionDeleted = event.data.object
+      canceledSub(subscriptionScheduleCanceled.customer)
       // Then define and call a function to handle the event customer.subscription.deleted
       break
-    case "subscription_schedule.canceled":
+    case "subscription_schedule.updated":
       const subscriptionScheduleCanceled = event.data.object
-      console.log("canceled: ", subscriptionScheduleCanceled)
+
       // Then define and call a function to handle the event subscription_schedule.canceled
       break
     // ... handle other event types
@@ -52,5 +55,5 @@ export default async function handler(req, res) {
   }
 
   // Return a 200 response to acknowledge receipt of the event
-  res.send()
+  res.send({ received: true })
 }
