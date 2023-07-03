e14d05997a6f8bd362e0921449738e7d740d3a13
Frank Marra <frankrmarra@gmail.com>
2023-07-03 14:04:47 -0400

    updated reset password validation

--- pages/reset-password.js
+++ pages/reset-password.js
@@ -8,8 +8,25 @@ export default function ResetPassword() {
   const [newPassword, setNewPassword] = useState("")
   const [newPasswordCheck, setNewPasswordCheck] = useState("")
   const [passwordUpdated, setPasswordUpdated] = useState(false)
+  const [passwordMessage, setPasswordMessage] = useState({
+    color: "transparent",
+    message: "",
+  })
+  const [passwordGood, setPasswordGood] = useState(false)
   const user = useUser()
   const router = useRouter()
+
+  const checkPassword = () => {
+    if (newPassword.length > 0) {
+      if (newPassword === newPasswordCheck) {
+        setPasswordMessage({ color: "green", message: "Passwords match!" })
+        setPasswordGood(true)
+      } else if (newPassword != newPasswordCheck) {
+        setPasswordMessage({ color: "red", message: "Passwords don't match!" })
+        setPasswordGood(false)
+      }
+    }
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -32,7 +49,10 @@ export default function ResetPassword() {
   }
 
   return passwordUpdated ? (
-    <div className="password-updated">
+    <div
+      className=" container stack inline-max center-stage password-updated"
+      style={{ "--max-inline-size": "400px" }}
+    >
       <h1>Password Updated</h1>
       <p>Please log in to access your dashboard.</p>
       <Link href="/">Log In</Link>
@@ -42,37 +62,51 @@ export default function ResetPassword() {
       className="container stack inline-max center-stage"
       style={{ "--max-inline-size": "400px" }}
     >
-      <h1>Reset Password</h1>
+      <h1>Update Password</h1>
 
       <form className="stack" onSubmit={handleSubmit}>
-        <label className="label" htmlFor="password">
-          New Password
-        </label>
-        <input
-          onChange={(e) => setNewPassword(e.target.value)}
-          className="input"
-          id="password"
-          type="password"
-          value={newPassword}
-          required
-        />
-
-        <label className="label" htmlFor="passwordcheck">
-          Verify New Password
-        </label>
-        <input
-          onChange={(e) => setNewPasswordCheck(e.target.value)}
-          className="input"
-          id="passwordcheck"
-          type="password"
-          value={newPasswordCheck}
-          required
-        />
+        <div>
+          <label className="label" htmlFor="password">
+            New Password
+          </label>
+          <input
+            onChange={(e) => setNewPassword(e.target.value)}
+            className="input"
+            id="password"
+            type="password"
+            value={newPassword}
+            required
+          />
+          <small>Password must be at least six characters long</small>
+        </div>
+        <div>
+          <label className="label" htmlFor="passwordcheck">
+            Verify New Password
+          </label>
+          <input
+            onChange={(e) => setNewPasswordCheck(e.target.value)}
+            className="input"
+            id="passwordcheck"
+            type="password"
+            value={newPasswordCheck}
+            onFocus={() =>
+              setPasswordMessage({ color: "transparent", message: "" })
+            }
+            disabled={newPassword.length < 6}
+            onBlur={checkPassword}
+            required
+          />
+          {
+            <small
+              style={{ color: `${passwordMessage.color}` }}
+            >{`${passwordMessage.message}`}</small>
+          }
+        </div>
         <button
           className="button"
           data-variant="primary"
           type="submit"
-          disabled={newPassword != newPasswordCheck || newPassword.length < 8}
+          disabled={!passwordGood}
         >
           Reset Password
         </button>
