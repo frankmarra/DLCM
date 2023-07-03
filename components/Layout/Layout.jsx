4d24823734a52dfec489126973fc2c547d76d0b0
Ryan Mulligan <hexagoncircle@gmail.com>
2023-07-03 13:04:59 -0700

    Add ThemeToggle component

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react"
 import Link from "next/link"
+import ThemeToggle from "@/components/ThemeToggle/ThemeToggle.jsx"
 import styles from "./Layout.module.css"
 
 export default function Layout({ children }) {
@@ -74,16 +75,27 @@ export default function Layout({ children }) {
       </header>
       <main className="inline-max stack">{children}</main>
       <footer className={styles.footer}>
-        <div className={styles.contact}>
-          <a href="mailto:dlcm.app@gmail.com">Contact Us</a>
-          <Link href="/privacy">Privacy Policy</Link>
-          <Link href="/terms">Terms and Conditions</Link>
-        </div>
+        <ul className={styles.contact} role="list">
+          <li>
+            <a href="mailto:dlcm.app@gmail.com">Contact Us</a>
+          </li>
+          <li>
+            <Link href="/privacy">Privacy Policy</Link>
+          </li>
+          <li>
+            <Link href="/terms">Terms and Conditions</Link>
+          </li>
+          <li>
+            <Link href="/signup">Sign up</Link>
+          </li>
+          <li>
+            <Link href="https://mysterycircles.com/donate-to-mc">
+              Buy us a coffee
+            </Link>
+          </li>
+        </ul>
         <div className={styles.copyright}>
-          <Link href="/signup">Join</Link>
-          <Link href="https://mysterycircles.com/donate-to-mc">
-            Buy us a coffee
-          </Link>
+          <ThemeToggle></ThemeToggle>
           <p>&copy; {`${date.getFullYear()}`} Mystery Circles</p>
         </div>
       </footer>
