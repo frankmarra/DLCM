5ccd546c47dbc3fa7c2b385d532030ef1e5d5752
Frank Marra <frankrmarra@gmail.com>
2023-07-06 19:48:38 -0400

    started implementing filter releases

--- components/Releases/Releases.jsx
+++ components/Releases/Releases.jsx
@@ -13,11 +13,34 @@ export default function Releases({ profileData }) {
   const [releases, setReleases] = useState([])
   const [allowNew, setAllowNew] = useState(true)
   const [addedNewRelease, setAddedNewRelease] = useState(false)
+  const [filter, setFilter] = useState("oldest")
 
   useEffect(() => {
     getReleases()
     setAddedNewRelease(false)
   }, [supabase, profileData.id, addedNewRelease])
+
+  useEffect(() => {
+    if (releases) {
+      let sortedReleases = releases
+      if (filter === "alphabetical") {
+        sortedReleases.sort((a, b) => {
+          let titleOne = a.title.toLowerCase()
+          let titleTwo = b.title.toLowerCase()
+
+          if (titleOne < titleTwo) {
+            return -1
+          }
+          if (titleOne > titleTwo) {
+            return 1
+          }
+          return 0
+        })
+      }
+
+      setReleases(sortedReleases)
+    }
+  }, [filter])
 
   async function getReleases() {
     try {
@@ -45,6 +68,14 @@ export default function Releases({ profileData }) {
     <article className="stack">
       <header className="article-heading inline-wrap">
         <h2>Releases</h2>
+        <div className={styles.filter}>
+          <button
+            className="button"
+            data-variant="primary"
+            type="button"
+            onClick={() => setFilter("alphabetical")}
+          >{`a -> z`}</button>
+        </div>
         {profileData.is_subscribed || profileData.dlcm_friend ? (
           <CreateRelease
             setAddedNewRelease={setAddedNewRelease}
