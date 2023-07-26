d6db2609262811fe17f14719159cdd3a894efe63
Frank Marra <frankrmarra@gmail.com>
2023-07-25 21:57:59 -0400

    cleaned up code

--- components/Pagination/Pagination.jsx
+++ components/Pagination/Pagination.jsx
@@ -6,10 +6,6 @@ const range = (start, end) => {
 }
 
 export default function Pagination({ pageCount, currentPage, setCurrentPage }) {
-  // const pageNumbers = [...Array(pageCount + 1).keys()].slice(1)
-  // pageNumbers.pop()
-  // pageNumbers.shift()
-  // let pages = []
   const getPageRange = (pageCount, currentPage) => {
     let first = Math.floor(5 / 2)
     let last = Math.ceil(5 / 2)
@@ -82,104 +78,4 @@ export default function Pagination({ pageCount, currentPage, setCurrentPage }) {
       )}
     </ul>
   )
-}
-
-{
-  /* <ul role="list" className={cn(styles.pages, "grid")}>
-    <li>
-      <a onClick={prevPage} href="#">
-        {"<<"}
-      </a>
-    </li>
-    {currentPage <= 3 ? (
-      <>
-        <li className={currentPage == 1 ? styles.active : ""}>
-          <a onClick={() => setCurrentPage(1)} href="#">
-            1
-          </a>
-        </li>
-        <li className={currentPage == 2 ? styles.active : ""}>
-          <a onClick={() => setCurrentPage(2)} href="#">
-            2
-          </a>
-        </li>
-        <li className={currentPage == 3 ? styles.active : ""}>
-          <a onClick={() => setCurrentPage(3)} href="#">
-            3
-          </a>
-        </li>
-        <li className={currentPage == 4 ? styles.active : ""}>
-          <a onClick={() => setCurrentPage(4)} href="#">
-            4
-          </a>
-        </li>
-        <li>...</li>
-        <li>
-          <a onClick={() => setCurrentPage(pages)}>{pages}</a>
-        </li>
-      </>
-    ) : null}
-    {currentPage >= 4 && currentPage <= pages - 3 ? (
-      <>
-        <li>
-          <a onClick={() => setCurrentPage(1)} href="#">
-            1
-          </a>
-        </li>
-        <li>...</li>
-        <li>
-          <a onClick={prevPage} href="#">
-            {currentPage - 1}
-          </a>
-        </li>
-        <li className={styles.active}>{currentPage}</li>
-        <li>
-          <a onClick={nextPage} href="#">
-            {currentPage + 1}
-          </a>
-        </li>
-        <li>...</li>
-        <li>
-          <a onClick={() => setCurrentPage(pages)} href="#">
-            {pages}
-          </a>
-        </li>
-      </>
-    ) : null}
-    {currentPage >= pages - 2 ? (
-      <>
-        <li>
-          <a onClick={() => setCurrentPage(1)}>1</a>
-        </li>
-        <li>...</li>
-
-        <li className={currentPage == pages - 3 ? styles.active : ""}>
-          <a onClick={() => setCurrentPage(pages - 3)} href="#">
-            {pages - 3}
-          </a>
-        </li>
-        <li className={currentPage == pages - 2 ? styles.active : ""}>
-          <a onClick={() => setCurrentPage(pages - 2)} href="#">
-            {pages - 2}
-          </a>
-        </li>
-        <li className={currentPage == pages - 1 ? styles.active : ""}>
-          <a onClick={() => setCurrentPage(pages - 1)} href="#">
-            {pages - 1}
-          </a>
-        </li>
-        <li className={currentPage == pages ? styles.active : ""}>
-          <a onClick={() => setCurrentPage(pages)} href="#">
-            {pages}
-          </a>
-        </li>
-      </>
-    ) : null}
-    <li>
-      <a onClick={nextPage} href="#">
-        {">>"}
-      </a>
-    </li>
-  </ul>
-*/
 }
