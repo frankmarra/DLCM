3f8c6edd61007f23c655b221e569ed7d9ea5a379
Ryan Mulligan <hexagoncircle@gmail.com>
2023-09-04 10:35:03 -0700

    Override quill theme to support light and dark modes

--- components/InputReleaseAbout/InputReleaseAbout.jsx
+++ components/InputReleaseAbout/InputReleaseAbout.jsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic"
-// import Quill from "react-quill"
 import "react-quill/dist/quill.snow.css"
+import styles from "./InputReleaseAbout.module.css"
 
 const QuillNoSSRWrapper = dynamic(import("react-quill"), {
   ssr: false,
@@ -23,6 +23,38 @@ export default function InputReleaseAbout({ about, setAbout }) {
       <label htmlFor="about" className="label">
         About this release
       </label>
+      <style>
+        {`
+          .ql-container {
+            font: inherit;
+            border-radius: 0 0 var(--radius-1) var(--radius-1);
+          }
+
+          .ql-toolbar {
+            border-radius: var(--radius-1) var(--radius-1) 0 0;
+          }
+
+          .ql-toolbar.ql-snow,
+          .ql-container.ql-snow {
+            border: var(--border-size-1) solid var(--surface-4);
+            box-shadow: var(--shadow-1);
+          }
+
+          .ql-snow .ql-fill,
+          .ql-snow .ql-stroke.ql-fill {
+            fill: currentColor !important;
+          }
+
+          .ql-snow .ql-stroke {
+            stroke: currentColor !important;
+            fill: none !important;
+          }
+
+          .ql-snow.ql-toolbar button:hover, .ql-snow .ql-toolbar button:hover, .ql-snow.ql-toolbar button:focus, .ql-snow .ql-toolbar button:focus, .ql-snow.ql-toolbar button.ql-active, .ql-snow .ql-toolbar button.ql-active, .ql-snow.ql-toolbar .ql-picker-label:hover, .ql-snow .ql-toolbar .ql-picker-label:hover, .ql-snow.ql-toolbar .ql-picker-label.ql-active, .ql-snow .ql-toolbar .ql-picker-label.ql-active, .ql-snow.ql-toolbar .ql-picker-item:hover, .ql-snow .ql-toolbar .ql-picker-item:hover, .ql-snow.ql-toolbar .ql-picker-item.ql-selected, .ql-snow .ql-toolbar .ql-picker-item.ql-selected {
+            color: var(--text-2);
+          }
+        `}
+      </style>
 
       <QuillNoSSRWrapper
         modules={modules}
